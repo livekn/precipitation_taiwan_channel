701550b60ba9f6e1434905d86de3213196988a9a
toby <me@livekn.com>
2017-08-04 11:41:09 +0800

    Fix photo caption with space bug.

--- precipitation_taiwan_bot.sh
+++ precipitation_taiwan_bot.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 photo1_url="http://www.cwb.gov.tw/V7/forecast/fcst/Data/QPF_ChFcstPrecip_6_06.jpg"
-photo1_caption="8~14"
+photo1_caption=`date "+%Y-%m-%d, 08:00 ~ 14:00"`
 photo2_url="http://www.cwb.gov.tw/V7/forecast/fcst/Data/QPF_ChFcstPrecip_6_12.jpg"
-photo2_caption="14~20"
+photo2_caption=`date "+%Y-%m-%d, 14:00 ~ 22:00"`
 
 if ! [[ -f config ]]
 then
@@ -24,6 +24,6 @@ request_url="https://api.telegram.org/bot"$token"/sendPhoto"
 photo1_url=$photo1_url"?time="`date +%s`	# so that Telegram won't use a cached version
 photo2_url=$photo2_url"?time="`date +%s`
 
-curl --url $request_url --data-urlencode 'chat_id='$chat_id --data-urlencode 'photo='$photo1_url --data-urlencode 'caption='$photo1_caption --data-urlencode 'time='`date +%s`
+curl --url $request_url --data-urlencode "chat_id=$chat_id" --data-urlencode "photo=$photo1_url" --data-urlencode "caption=$photo1_caption" --data-urlencode "time=`date +%s`"
 
-curl --url $request_url --header 'content-type: application/x-www-form-urlencoded' --data-urlencode 'chat_id='$chat_id --data-urlencode 'photo='$photo2_url --data-urlencode 'caption='$photo2_caption --data-urlencode 'time='`date +%s`
+curl --url $request_url --data-urlencode "chat_id=$chat_id" --data-urlencode "photo=$photo2_url" --data-urlencode "caption=$photo2_caption" --data-urlencode "time=`date +%s`"
